0bc001953c9be9a5ed76b9c30baf3d0efdf0f156
Stanislav Kruglov <erik2688@gmail.com>
2015-07-16 18:04:44 +0300

    feature: Added language

---
+++ SuhovTask/Core/Configuration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Resources;
+using System.Reflection;
+
+namespace SuhovTask.Core
+{
+    public static class LocalConfiguration
+    {
+        private static string _language = null;
+
+        private static ResourceManager _res;
+
+        private static bool handleChange = true;
+
+        public static string Language
+        {
+            get
+            {
+                if (_language == null)
+                    _language = ConfigurationManager.AppSettings["Language"];
+                return _language;
+            }
+            set 
+            {
+                handleChange = false;
+                _language = value;
+            }
+        }
+
+        public static ResourceManager Resource
+        {
+            get
+            {
+                if ((_res == null) || (!handleChange))
+                {
+                    _res = new ResourceManager(Language, Assembly.GetExecutingAssembly());
+                    handleChange = true;
+                };
+                return _res;
+            }
+        }
+    }
+}
--- SuhovTask/Program.cs
+++ SuhovTask/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Resources;
+using System.Configuration;
+using SuhovTask.Core;
+using SuhovTask.LanguageFiles;
 
 namespace SuhovTask
 {
     class Program
     {
+        ResourceManager res_man;    
+
         static void Main(string[] args)
         {
+            string numberAsString;
+            int number = 0;
+            numberAsString = Console.ReadLine();
+
+            try
+            {
+                number = Convert.ToInt32(numberAsString);
+                string Text = res_man.GetString("MainForm_text", LocalConfiguration.Language);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Input string is not a sequence of digits.");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("The number cannot fit in an Int32.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.Write("The number is: {0}", number.ToString());
+                Console.ReadLine();
+            }
+
         }
     }
 }
